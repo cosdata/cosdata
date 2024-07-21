e257fa2966bc81588348657c9a74a8f92f2b6f5b
Dilshad <a-rustacean@outlook.com>
2024-07-21 21:09:17 +0530

    chore: make it compile

--- src/api/vectordb/transactions/create.rs
+++ src/api/vectordb/transactions/create.rs
@@ -1,17 +1,16 @@
+use crate::models::types::get_app_env;
 use actix_web::{web, HttpResponse};
 
-use crate::web_server::AppEnv;
-
 // Route: `/vectordb/{database_name}/transactions`
-pub(crate) async fn create(
-    env: web::Data<AppEnv>,
-    database_name: web::Path<String>,
-) -> HttpResponse {
+pub(crate) async fn create(database_name: web::Path<String>) -> HttpResponse {
+    let env = match get_app_env() {
+        Ok(env) => env,
+        Err(_) => return HttpResponse::InternalServerError().body("Env initialization error"),
+    };
+
     let Some(vec_store) = env.vector_store_map.get(&database_name.into_inner()) else {
         return HttpResponse::NotFound().body("Vector store not found");
     };
-
-    vec_store.levels_prob;
 
     todo!()
 }
--- src/api/vectordb/transactions/upsert.rs
+++ src/api/vectordb/transactions/upsert.rs
@@ -1,13 +1,13 @@
+use crate::models::types::get_app_env;
 use actix_web::{web, HttpResponse};
 
-use crate::web_server::AppEnv;
-
 // Route: `/vectordb/{database_name}/transactions/{transaction_id}/upsert`
-pub(crate) async fn upsert(
-    env: web::Data<AppEnv>,
-    path_data: web::Path<(String, String)>,
-) -> HttpResponse {
+pub(crate) async fn upsert(path_data: web::Path<(String, String)>) -> HttpResponse {
     let (database_name, transaction_id) = path_data.into_inner();
+    let env = match get_app_env() {
+        Ok(env) => env,
+        Err(_) => return HttpResponse::InternalServerError().body("Env initialization error"),
+    };
     let Some(vec_store) = env.vector_store_map.get(&database_name) else {
         return HttpResponse::NotFound().body("Vector store not found");
     };
