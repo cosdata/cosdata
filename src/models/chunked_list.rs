88bdacdcc13f45825654936f77d0ed15b92956b3
Dilshad <a-rustacean@outlook.com>
2024-08-09 05:10:39 +0530

    feat: impl `CustomSerialize` for `EagerLazyItemSet`

--- src/models/chunked_list.rs
+++ src/models/chunked_list.rs
@@ -1,7 +1,9 @@
 use arcshift::ArcShift;
 
+use super::serializer::CustomSerialize;
 use super::types::Item;
-use std::sync::{Arc, RwLock};
+use std::collections::HashMap;
+use std::hash::Hash;
 
 pub trait SyncPersist {
     fn set_persistence(&self, flag: bool);
@@ -14,7 +16,85 @@ type HNSWLevel = u8;
 
 pub const CHUNK_SIZE: usize = 5;
 
-// #[derive(Debug, Clone)]
+pub trait Identifiable {
+    type Id: Eq + Hash;
+    fn get_id(&self) -> Self::Id;
+}
+
+#[derive(Debug, Clone)]
+pub struct IdentitySet<T: Identifiable> {
+    map: HashMap<T::Id, T>,
+}
+
+impl<T: Identifiable> IdentitySet<T> {
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn from_iter(iter: impl Iterator<Item = T>) -> Self {
+        Self {
+            map: iter.map(|item| (item.get_id(), item)).collect(),
+        }
+    }
+
+    pub fn insert(&mut self, value: T) -> Option<T> {
+        self.map.insert(value.get_id(), value)
+    }
+
+    pub fn contains(&self, value: &T) -> bool {
+        self.map.contains_key(&value.get_id())
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        self.map.iter().map(|(_, value)| value)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.map.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub enum IdentityMapKey {
+    String(String),
+    Int(u32),
+}
+
+#[derive(Debug, Clone)]
+pub struct IdentityMap<T> {
+    map: HashMap<IdentityMapKey, T>,
+}
+
+impl<T> IdentityMap<T> {
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, key: IdentityMapKey, value: T) -> Option<T> {
+        self.map.insert(key, value)
+    }
+
+    pub fn contains(&self, key: &IdentityMapKey) -> bool {
+        self.map.contains_key(key)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.map.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
+}
+
 #[derive(Clone)]
 pub enum LazyItem<T: Clone + 'static> {
     Valid {
@@ -25,20 +105,84 @@ pub enum LazyItem<T: Clone + 'static> {
     Invalid,
 }
 
-// #[derive(Debug, Clone)]
 #[derive(Clone)]
-pub struct LazyItemRef<T: Clone + 'static> {
+pub struct EagerLazyItem<T: Clone + 'static, E: Clone + CustomSerialize + 'static>(
+    pub E,
+    pub LazyItem<T>,
+);
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+pub enum LazyItemId {
+    Memory(u64),
+    Persist(u32),
+}
+
+impl<T> Identifiable for LazyItem<T>
+where
+    T: Clone + Identifiable<Id = u64> + 'static,
+{
+    type Id = LazyItemId;
+
+    fn get_id(&self) -> Self::Id {
+        if let LazyItem::Valid { data, offset, .. } = self {
+            if let Some(offset) = offset {
+                return LazyItemId::Persist(*offset);
+            }
+
+            if let Some(data) = data {
+                let mut arc = data.clone();
+                return LazyItemId::Memory(arc.get().get_id());
+            }
+        }
+
+        LazyItemId::Persist(u32::MAX)
+    }
+}
+
+impl<T, E> Identifiable for EagerLazyItem<T, E>
+where
+    T: Clone + Identifiable<Id = u64> + 'static,
+    E: Clone + CustomSerialize + 'static,
+{
+    type Id = LazyItemId;
+
+    fn get_id(&self) -> Self::Id {
+        self.1.get_id()
+    }
+}
+
+#[derive(Clone)]
+pub struct LazyItemRef<T>
+where
+    T: Clone + 'static,
+{
     pub item: Item<LazyItem<T>>,
 }
 
-// #[derive(Debug, Clone)]
 #[derive(Clone)]
-pub struct LazyItems<T: Clone + 'static> {
-    pub items: Item<Vec<LazyItemRef<T>>>,
+pub struct EagerLazyItemSet<T, E>
+where
+    T: Clone + Identifiable<Id = u64> + 'static,
+    E: Clone + CustomSerialize + 'static,
+{
+    pub items: Item<IdentitySet<EagerLazyItem<T, E>>>,
+}
+
+#[derive(Clone)]
+pub struct LazyItemSet<T>
+where
+    T: Clone + Identifiable<Id = u64> + 'static,
+{
+    pub items: Item<IdentitySet<LazyItem<T>>>,
+}
+
+#[derive(Clone)]
+pub struct LazyItemMap<T: Clone + 'static> {
+    pub items: Item<IdentityMap<LazyItem<T>>>,
 }
 
 impl<T: Clone> LazyItem<T> {
-    pub fn with_data(data: T) -> Self {
+    pub fn from_data(data: T) -> Self {
         LazyItem::Valid {
             data: Some(Item::new(data)),
             offset: None,
@@ -47,14 +191,61 @@ impl<T: Clone> LazyItem<T> {
     }
 }
 
-impl<T: Clone> LazyItemRef<T> {
+impl<T: Clone + 'static> LazyItem<T> {
+    pub fn new(item: T) -> Self {
+        Self::Valid {
+            data: Some(Item::new(item)),
+            offset: None,
+            decay_counter: 0,
+        }
+    }
+
+    pub fn new_invalid() -> Self {
+        Self::Invalid
+    }
+
+    pub fn from_item(item: Item<T>) -> Self {
+        Self::Valid {
+            data: Some(item),
+            offset: None,
+            decay_counter: 0,
+        }
+    }
+
+    pub fn get_data(&self) -> Option<Item<T>> {
+        if let Self::Valid { data, .. } = self {
+            return data.clone();
+        }
+        None
+    }
+
+    pub fn set_data(&mut self, new_data: T) {
+        if let Self::Valid { data, .. } = self {
+            *data = Some(Item::new(new_data))
+        }
+    }
+
+    pub fn set_offset(&mut self, new_offset: Option<FileOffset>) {
+        if let Self::Valid { offset, .. } = self {
+            *offset = new_offset;
+        }
+    }
+}
+
+impl<T: Clone + 'static> LazyItemRef<T> {
     pub fn new(item: T) -> Self {
         LazyItemRef {
             item: ArcShift::new(LazyItem::Valid {
-                data: Some(ArcShift::new(item)),
+                data: Some(Item::new(item)),
                 offset: None,
                 decay_counter: 0,
             }),
+        }
+    }
+
+    pub fn new_invalid() -> Self {
+        LazyItemRef {
+            item: Item::new(LazyItem::Invalid),
         }
     }
 
@@ -68,15 +259,18 @@ impl<T: Clone> LazyItemRef<T> {
         }
     }
 
-    pub fn get_data(&mut self) -> Option<Item<T>> {
-        if let LazyItem::Valid { data, .. } = self.item.get() {
+    pub fn get_data(&self) -> Option<Item<T>> {
+        let mut arc = self.item.clone();
+        if let LazyItem::Valid { data, .. } = arc.get() {
             return data.clone();
         }
         None
     }
 
-    pub fn set_data(&mut self, new_data: T) {
-        self.item.rcu(|item| {
+    pub fn set_data(&self, new_data: T) {
+        let mut arc = self.item.clone();
+
+        arc.rcu(|item| {
             let (offset, decay_counter) = if let LazyItem::Valid {
                 offset,
                 decay_counter,
@@ -116,34 +310,90 @@ impl<T: Clone> LazyItemRef<T> {
     }
 }
 
-impl<T: Clone> LazyItems<T> {
+impl<T, E> EagerLazyItemSet<T, E>
+where
+    T: Clone + Identifiable<Id = u64> + 'static,
+    E: Clone + CustomSerialize + 'static,
+{
     pub fn new() -> Self {
-        LazyItems {
-            items: Item::new(Vec::new()),
+        Self {
+            items: Item::new(IdentitySet::new()),
         }
     }
 
-    pub fn push(&mut self, item: LazyItemRef<T>) {
-        self.items.rcu(|items| {
-            let mut items = items.clone();
-            items.push(item);
-            items
-        });
+    pub fn insert(&self, item: LazyItem<T>, eager_data: E) {
+        let mut arc = self.items.clone();
+
+        arc.rcu(|set| {
+            let mut set = set.clone();
+            set.insert(EagerLazyItem(eager_data, item));
+            set
+        })
     }
 
-    pub fn get(&mut self, index: usize) -> Option<LazyItemRef<T>> {
-        self.items.get().get(index).cloned()
+    pub fn is_empty(&self) -> bool {
+        let mut arc = self.items.clone();
+        arc.get().is_empty()
     }
 
-    pub fn len(&mut self) -> usize {
-        self.items.get().len()
+    pub fn len(&self) -> usize {
+        let mut arc = self.items.clone();
+        arc.get().len()
+    }
+}
+
+impl<T: Clone + Identifiable<Id = u64> + 'static> LazyItemSet<T> {
+    pub fn new() -> Self {
+        Self {
+            items: Item::new(IdentitySet::new()),
+        }
     }
 
-    pub fn is_empty(&mut self) -> bool {
-        self.items.get().is_empty()
+    pub fn insert(&self, item: LazyItem<T>) {
+        let mut arc = self.items.clone();
+
+        arc.rcu(|set| {
+            let mut set = set.clone();
+            set.insert(item);
+            set
+        })
     }
 
-    pub fn iter(&mut self) -> impl Iterator<Item = LazyItemRef<T>> {
-        self.items.get().clone().into_iter()
+    pub fn is_empty(&self) -> bool {
+        let mut arc = self.items.clone();
+        arc.get().is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        let mut arc = self.items.clone();
+        arc.get().len()
+    }
+}
+
+impl<T: Clone + Identifiable<Id = u64> + 'static> LazyItemMap<T> {
+    pub fn new() -> Self {
+        Self {
+            items: Item::new(IdentityMap::new()),
+        }
+    }
+
+    pub fn insert(&self, key: IdentityMapKey, value: LazyItem<T>) {
+        let mut arc = self.items.clone();
+
+        arc.rcu(|set| {
+            let mut map = set.clone();
+            map.insert(key, value);
+            map
+        })
+    }
+
+    pub fn is_empty(&self) -> bool {
+        let mut arc = self.items.clone();
+        arc.get().is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        let mut arc = self.items.clone();
+        arc.get().len()
     }
 }
--- src/models/types.rs
+++ src/models/types.rs
@@ -16,9 +16,12 @@ use lmdb::{Database, Environment};
 use serde::{Deserialize, Serialize};
 use std::fmt;
 use std::fs::*;
-use std::hash::Hash;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use std::path::Path;
-use std::sync::{Arc, OnceLock, RwLock};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc, OnceLock,
+};
 
 pub type HNSWLevel = u8;
 pub type FileOffset = u32;
@@ -28,10 +31,30 @@ pub type CosineSimilarity = f32;
 
 pub type Item<T> = ArcShift<T>;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Neighbour {
     pub node: LazyItem<MergedNode>,
     pub cosine_similarity: CosineSimilarity,
+}
+
+impl Identifiable for Neighbour {
+    type Id = LazyItemId;
+
+    fn get_id(&self) -> Self::Id {
+        self.node.get_id()
+    }
+}
+
+impl Identifiable for MergedNode {
+    type Id = u64;
+
+    fn get_id(&self) -> Self::Id {
+        let mut prop_ref = self.prop.clone();
+        let prop = prop_ref.get();
+        let mut hasher = DefaultHasher::new();
+        prop.hash(&mut hasher);
+        hasher.finish()
+    }
 }
 
 pub type PropPersistRef = (FileOffset, BytesToRead);
@@ -44,7 +67,16 @@ pub struct NodeProp {
     pub location: Option<PropPersistRef>,
 }
 
-#[derive(Debug, Clone)]
+impl Hash for NodeProp {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher,
+    {
+        self.id.hash(state);
+    }
+}
+
+#[derive(Debug, Clone, Hash)]
 pub enum PropState {
     Ready(Arc<NodeProp>),
     Pending(PropPersistRef),
@@ -56,17 +88,16 @@ pub enum VectorId {
     Int(i32),
 }
 
-// #[derive(Debug, Clone)]
 #[derive(Clone)]
 pub struct MergedNode {
     pub version_id: VersionId,
     pub hnsw_level: HNSWLevel,
     pub prop: Item<PropState>,
-    pub neighbors: LazyItems<Neighbour>,
-    pub parent: Option<LazyItemRef<MergedNode>>,
-    pub child: Option<LazyItemRef<MergedNode>>,
-    pub versions: LazyItems<MergedNode>,
-    pub persist_flag: Item<bool>,
+    pub neighbors: EagerLazyItemSet<MergedNode, f32>,
+    pub parent: LazyItemRef<MergedNode>,
+    pub child: LazyItemRef<MergedNode>,
+    pub versions: LazyItemMap<MergedNode>,
+    pub persist_flag: Arc<AtomicBool>,
 }
 
 #[derive(Debug)]
@@ -119,141 +150,101 @@ impl MergedNode {
             version_id,
             hnsw_level,
             prop: Item::new(PropState::Pending((0, 0))),
-            neighbors: LazyItems::new(),
-            parent: None,
-            child: None,
-            versions: LazyItems::new(),
-            persist_flag: Item::new(true),
+            neighbors: EagerLazyItemSet::new(),
+            parent: LazyItemRef::new_invalid(),
+            child: LazyItemRef::new_invalid(),
+            versions: LazyItemMap::new(),
+            persist_flag: Arc::new(AtomicBool::new(true)),
         }
     }
 
     pub fn add_ready_neighbor(&self, neighbor: LazyItem<MergedNode>, cosine_similarity: f32) {
-        let neighbor_ref = Item::new(Neighbour {
-            node: neighbor,
-            cosine_similarity,
-        });
-        let lazy_item = LazyItem {
-            data: Some(neighbor_ref),
-            offset: None,
-            decay_counter: 0,
-        };
-        let mut neighbours = self.neighbors.clone();
-        neighbours.push(lazy_item);
+        self.neighbors.insert(neighbor, cosine_similarity);
     }
 
-    pub fn set_parent(mut node: ArcShift<Self>, parent: Option<LazyItemRef<MergedNode>>) {
-        node.rcu(|node| MergedNode {
-            parent,
-            ..node.clone()
-        })
+    pub fn set_parent(&self, parent: LazyItem<MergedNode>) {
+        let mut arc = self.parent.item.clone();
+        arc.update(parent);
     }
 
-    pub fn set_child(&mut self, child: Option<LazyItemRef<MergedNode>>) {
-        self.child = child;
+    pub fn set_child(&self, child: LazyItem<MergedNode>) {
+        let mut arc = self.child.item.clone();
+        arc.update(child);
     }
 
-    pub fn add_ready_neighbors(&mut self, neighbors_list: Vec<(LazyItem<MergedNode>, f32)>) {
+    pub fn add_ready_neighbors(&self, neighbors_list: Vec<(LazyItem<MergedNode>, f32)>) {
         for (neighbor, cosine_similarity) in neighbors_list {
             self.add_ready_neighbor(neighbor, cosine_similarity);
         }
     }
 
-    pub fn get_neighbors(&mut self) -> Vec<LazyItemRef<Neighbour>> {
-        self.neighbors.items.get().clone()
+    pub fn get_neighbors(&self) -> EagerLazyItemSet<MergedNode, f32> {
+        self.neighbors.clone()
     }
 
-    pub fn set_neighbors(&mut self, new_neighbors: Vec<LazyItem<Neighbour>>) {
-        self.neighbors.items.update(new_neighbors);
-    }
+    // pub fn set_neighbors(&self, new_neighbors: IdentitySet<EagerLazyItem<MergedNode, f32>>) {
+    //     let mut arc = self.neighbors.items.clone();
+    //     arc.update(new_neighbors);
+    // }
 
-    pub fn add_version(&mut self, version: Item<MergedNode>) {
-        let lazy_item = LazyItem {
+    pub fn add_version(&self, version: Item<MergedNode>) {
+        let lazy_item = LazyItem::Valid {
             data: Some(version),
             offset: None,
             decay_counter: 0,
         };
-        self.versions.push(lazy_item);
+        // TODO: look at the id
+        self.versions.insert(IdentityMapKey::Int(0), lazy_item);
     }
 
-    pub fn get_versions(&mut self) -> Vec<LazyItem<MergedNode>> {
-        self.versions.items.get().clone()
+    pub fn get_versions(&self) -> LazyItemMap<MergedNode> {
+        self.versions.clone()
     }
 
-    pub fn get_parent(&self) -> Option<LazyItemRef<MergedNode>> {
+    pub fn get_parent(&self) -> LazyItemRef<MergedNode> {
         self.parent.clone()
     }
 
-    pub fn get_child(&self) -> Option<LazyItemRef<MergedNode>> {
+    pub fn get_child(&self) -> LazyItemRef<MergedNode> {
         self.child.clone()
     }
 
-    pub fn set_prop_location(&mut self, new_location: PropPersistRef) {
-        let mut prop = self.prop.write().unwrap();
-        *prop = PropState::Pending(new_location);
+    pub fn set_prop_location(&self, new_location: PropPersistRef) {
+        let mut arc = self.prop.clone();
+        arc.update(PropState::Pending(new_location));
     }
 
     pub fn get_prop_location(&self) -> Option<PropPersistRef> {
-        let prop = self.prop.read().unwrap();
-        match *prop {
+        let mut arc = self.prop.clone();
+        match arc.get() {
             PropState::Ready(ref node_prop) => node_prop.location,
-            PropState::Pending(location) => Some(location),
+            PropState::Pending(location) => Some(*location),
         }
     }
 
     pub fn get_prop(&self) -> PropState {
-        self.prop.read().unwrap().clone()
+        let mut arc = self.prop.clone();
+        arc.get().clone()
     }
 
     pub fn set_prop_pending(&self, prop_ref: PropPersistRef) {
-        let mut prop = self.prop.write().unwrap();
-        *prop = PropState::Pending(prop_ref);
+        let mut arc = self.prop.clone();
+        arc.update(PropState::Pending(prop_ref));
     }
 
     pub fn set_prop_ready(&self, node_prop: Arc<NodeProp>) {
-        let mut prop = self.prop.write().unwrap();
-        *prop = PropState::Ready(node_prop);
-    }
-
-    pub fn set_persistence(&self, flag: bool) {
-        let mut fl = self.persist_flag.write().unwrap();
-        *fl = flag;
-    }
-
-    pub fn needs_persistence(&self) -> bool {
-        let fl = self.persist_flag.read().unwrap();
-        *fl
+        let mut arc = self.prop.clone();
+        arc.update(PropState::Ready(node_prop));
     }
 }
 
 impl SyncPersist for MergedNode {
     fn set_persistence(&self, flag: bool) {
-        let mut fl = self.persist_flag.write().unwrap();
-        *fl = flag;
+        self.persist_flag.store(flag, Ordering::Relaxed);
     }
 
     fn needs_persistence(&self) -> bool {
-        let fl = self.persist_flag.read().unwrap();
-        *fl
-    }
-}
-
-impl SyncPersist for Neighbour {
-    fn set_persistence(&self, flag: bool) {
-        let Some(node) = self.node.data.clone() else {
-            return;
-        };
-        let guard = node.read().unwrap();
-        let mut fl = guard.persist_flag.write().unwrap();
-        *fl = flag;
-    }
-
-    fn needs_persistence(&self) -> bool {
-        let Some(node) = self.node.data.clone() else {
-            return false;
-        };
-        let guard = node.read().unwrap();
-        let fl = guard.persist_flag.read().unwrap();
-        *fl
+        self.persist_flag.load(Ordering::Relaxed)
     }
 }
 
@@ -266,19 +257,21 @@ impl fmt::Display for VectorId {
         }
     }
 }
-impl fmt::Display for MergedNode {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "MergedNode {{ version_id: {}, hnsw_level: {}, prop: {:?}, neighbors: {:?}, parent: {:?}, child: {:?}, version_ref: {:?} }}",
-            self.version_id,
-            self.hnsw_level,
-            self.prop.read().unwrap(),
-            self.neighbors,
-            self.parent,
-            self.child,
-            self.versions
-        )
-    }
-}
+
+// impl fmt::Display for MergedNode {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "MergedNode {{ version_id: {}, hnsw_level: {}, prop: {:?}, neighbors: {:?}, parent: {:?}, child: {:?}, version_ref: {:?} }}",
+//             self.version_id,
+//             self.hnsw_level,
+//             self.prop.read().unwrap(),
+//             self.neighbors,
+//             self.parent,
+//             self.child,
+//             self.versions
+//         )
+//     }
+// }
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum VectorQt {
     UnsignedByte {
@@ -321,7 +314,7 @@ pub struct MetaDb {
     pub db: Arc<Database>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct VectorStore {
     pub exec_queue_nodes: ExecQueueUpdate,
     pub max_cache_level: u8,
@@ -363,31 +356,22 @@ impl VectorStore {
             prop_file,
             version_lmdb,
             current_version,
-            current_open_transaction: Arc::new(RwLock::new(None)),
+            current_open_transaction: Item::new(None),
             quantization_metric,
             distance_metric,
             storage_type,
         }
     }
     // Get method
-    pub fn get_current_version(
-        &self,
-    ) -> Result<
-        Option<VersionHash>,
-        std::sync::PoisonError<std::sync::RwLockReadGuard<'_, Option<VersionHash>>>,
-    > {
-        self.current_version.read().map(|guard| guard.clone())
+    pub fn get_current_version(&self) -> Option<VersionHash> {
+        let mut arc = self.current_version.clone();
+        arc.get().clone()
     }
 
     // Set method
-    pub fn set_current_version(
-        &self,
-        new_version: Option<VersionHash>,
-    ) -> Result<(), std::sync::PoisonError<std::sync::RwLockWriteGuard<'_, Option<VersionHash>>>>
-    {
-        let mut write_guard = self.current_version.write()?;
-        *write_guard = new_version;
-        Ok(())
+    pub fn set_current_version(&self, new_version: Option<VersionHash>) {
+        let mut arc = self.current_version.clone();
+        arc.update(new_version);
     }
 }
 #[derive(Debug, Clone)]
