5eacc6a50daf2dbe1cf1382b212cc2ff43bfaf14
Dilshad <a-rustacean@outlook.com>
2024-08-11 20:32:52 +0530

    chore: add unit tests for `AttributeDefinition` and `AttributeDefinitions` parsing

--- src/cosql/definition/mod.rs
+++ src/cosql/definition/mod.rs
@@ -19,7 +19,7 @@ pub use relationship::RelationshipDefinition;
 
 pub type AttributeDefinitions = Vec<AttributeDefinition>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct AttributeDefinition {
     pub name: String,
     pub data_type: DataType,
@@ -41,4 +41,92 @@ pub fn parse_attribute_definition(input: &str) -> IResult<&str, AttributeDefinit
             data_type,
         },
     )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_attribute_definition_parser() {
+        let values = [
+            (
+                "name: string",
+                AttributeDefinition {
+                    name: "name".to_string(),
+                    data_type: DataType::String,
+                },
+            ),
+            (
+                "age: int",
+                AttributeDefinition {
+                    name: "age".to_string(),
+                    data_type: DataType::Int,
+                },
+            ),
+            (
+                "date_of_birth: date",
+                AttributeDefinition {
+                    name: "date_of_birth".to_string(),
+                    data_type: DataType::Date,
+                },
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_attribute_definition(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
+
+    #[test]
+    fn test_attribute_definitions_parser() {
+        let values = [
+            (
+                "name: string,
+                age: int,
+                date_of_birth: date",
+                vec![
+                    AttributeDefinition {
+                        name: "name".to_string(),
+                        data_type: DataType::String,
+                    },
+                    AttributeDefinition {
+                        name: "age".to_string(),
+                        data_type: DataType::Int,
+                    },
+                    AttributeDefinition {
+                        name: "date_of_birth".to_string(),
+                        data_type: DataType::Date,
+                    },
+                ],
+            ),
+            (
+                "name: string,
+                start_date: date,
+                end_date: date",
+                vec![
+                    AttributeDefinition {
+                        name: "name".to_string(),
+                        data_type: DataType::String,
+                    },
+                    AttributeDefinition {
+                        name: "start_date".to_string(),
+                        data_type: DataType::Date,
+                    },
+                    AttributeDefinition {
+                        name: "end_date".to_string(),
+                        data_type: DataType::Date,
+                    },
+                ],
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_attribute_definitions1(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
 }
