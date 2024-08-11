8bc48df6ce81c609e62d2b4bac7b27c3196a226d
Dilshad <a-rustacean@outlook.com>
2024-08-11 20:33:58 +0530

    chore: add unit test for `EntityDefinition` parsing

--- src/cosql/definition/entity.rs
+++ src/cosql/definition/entity.rs
@@ -5,7 +5,7 @@ use nom::{
 use super::{parse_attribute_definitions1, AttributeDefinitions};
 use crate::cosql::common::{parse_identifier, ws};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct EntityDefinition {
     pub name: String,
     pub attributes: AttributeDefinitions,
@@ -24,4 +24,69 @@ pub fn parse_entity_definition(input: &str) -> IResult<&str, EntityDefinition> {
             attributes,
         },
     )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cosql::{definition::AttributeDefinition, DataType};
+
+    #[test]
+    fn test_entity_definition_parser() {
+        // the `parse_entity_definition` function assumes the `define entity` part is already consumed
+        let values = [
+            (
+                "person as
+                    name: string,
+                    age: int,
+                    date_of_birth: date;",
+                EntityDefinition {
+                    name: "person".to_string(),
+                    attributes: vec![
+                        AttributeDefinition {
+                            name: "name".to_string(),
+                            data_type: DataType::String,
+                        },
+                        AttributeDefinition {
+                            name: "age".to_string(),
+                            data_type: DataType::Int,
+                        },
+                        AttributeDefinition {
+                            name: "date_of_birth".to_string(),
+                            data_type: DataType::Date,
+                        },
+                    ],
+                },
+            ),
+            (
+                "project as
+                    name: string,
+                    start_date: date,
+                    end_date: date;",
+                EntityDefinition {
+                    name: "project".to_string(),
+                    attributes: vec![
+                        AttributeDefinition {
+                            name: "name".to_string(),
+                            data_type: DataType::String,
+                        },
+                        AttributeDefinition {
+                            name: "start_date".to_string(),
+                            data_type: DataType::Date,
+                        },
+                        AttributeDefinition {
+                            name: "end_date".to_string(),
+                            data_type: DataType::Date,
+                        },
+                    ],
+                },
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_entity_definition(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
 }
