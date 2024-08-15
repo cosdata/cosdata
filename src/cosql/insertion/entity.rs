2859064c109075008ab5aa6aebaad2b7f0f333dc
Dilshad <a-rustacean@outlook.com>
2024-08-15 13:23:41 +0530

    chore: add tests for `EntityInsertion` parser

--- src/cosql/insertion/entity.rs
+++ src/cosql/insertion/entity.rs
@@ -6,7 +6,7 @@ use crate::cosql::common::{parse_identifier, ws};
 
 use super::{parse_attributes1, Attributes};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct EntityInsertion {
     pub variable: String,
     pub entity_type: String,
@@ -29,4 +29,72 @@ pub fn parse_entity_insertion(input: &str) -> IResult<&str, EntityInsertion> {
             attributes,
         },
     )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cosql::{insertion::Attribute, Value};
+
+    #[test]
+    fn test_entity_insertion_parser() {
+        let values = [
+            (
+                r#"$rust_dev isa person (
+                    name: "The Rust Dev",
+                    age: 54,
+                    date_of_birth: 01-01-1970
+                );"#,
+                EntityInsertion {
+                    variable: "rust_dev".to_string(),
+                    entity_type: "person".to_string(),
+                    attributes: vec![
+                        Attribute {
+                            name: "name".to_string(),
+                            value: Value::String("The Rust Dev".to_string()),
+                        },
+                        Attribute {
+                            name: "age".to_string(),
+                            value: Value::Int(54),
+                        },
+                        Attribute {
+                            name: "date_of_birth".to_string(),
+                            value: Value::Date("01-01-1970".to_string()),
+                        },
+                    ],
+                },
+            ),
+            (
+                r#"$rust_project isa project (
+                    name: "A Rust Project",
+                    start_date: 01-01-2000,
+                    end_date: 31-12-2009 
+                );"#,
+                EntityInsertion {
+                    variable: "rust_project".to_string(),
+                    entity_type: "project".to_string(),
+                    attributes: vec![
+                        Attribute {
+                            name: "name".to_string(),
+                            value: Value::String("A Rust Project".to_string()),
+                        },
+                        Attribute {
+                            name: "start_date".to_string(),
+                            value: Value::Date("01-01-2000".to_string()),
+                        },
+                        Attribute {
+                            name: "end_date".to_string(),
+                            value: Value::Date("31-12-2009".to_string()),
+                        },
+                    ],
+                },
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_entity_insertion(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
 }
