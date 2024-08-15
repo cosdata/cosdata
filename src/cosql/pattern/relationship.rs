4c63039a389687aabb7b2aa6b6da8f787a54c5c4
Dilshad <a-rustacean@outlook.com>
2024-08-15 13:22:26 +0530

    chore: add tests for `RoleAssignment` and `RoleAssignments` parsers

--- src/cosql/pattern/relationship.rs
+++ src/cosql/pattern/relationship.rs
@@ -8,13 +8,13 @@ use nom::{
 };
 
 use crate::cosql::{
-    common::{parse_identifier, ws},
+    common::{parse_identifier, parse_variable, ws},
     insertion::{parse_attributes0, Attributes},
 };
 
 pub type RoleAssignments = Vec<RoleAssignment>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RoleAssignment {
     pub role: String,
     pub entity: String,
@@ -30,13 +30,8 @@ pub struct RelationshipPattern {
 
 pub fn parse_role_assignment(input: &str) -> IResult<&str, RoleAssignment> {
     map(
-        tuple((
-            ws(parse_identifier),
-            ws(char(':')),
-            ws(char('$')),
-            ws(parse_identifier),
-        )),
-        |(role, _, _, entity)| RoleAssignment {
+        tuple((ws(parse_identifier), ws(char(':')), ws(parse_variable))),
+        |(role, _, entity)| RoleAssignment {
             role: role.to_string(),
             entity: entity.to_string(),
         },
@@ -67,4 +62,79 @@ pub fn parse_relationship_pattern(input: &str) -> IResult<&str, RelationshipPatt
             attributes: attributes.unwrap_or_default(),
         },
     )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_role_assignment_parser() {
+        let values = [
+            (
+                "project: $rust_project",
+                RoleAssignment {
+                    role: "project".to_string(),
+                    entity: "rust_project".to_string(),
+                },
+            ),
+            (
+                "assignee: $rust_dev",
+                RoleAssignment {
+                    role: "assignee".to_string(),
+                    entity: "rust_dev".to_string(),
+                },
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_role_assignment(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
+
+    #[test]
+    fn test_role_assignments_parser() {
+        let values = [
+            (
+                "(
+                    project: $rust_project,
+                    assignee: $rust_dev
+                )",
+                vec![
+                    RoleAssignment {
+                        role: "project".to_string(),
+                        entity: "rust_project".to_string(),
+                    },
+                    RoleAssignment {
+                        role: "assignee".to_string(),
+                        entity: "rust_dev".to_string(),
+                    },
+                ],
+            ),
+            (
+                "(
+                    employee: $rust_dev,
+                    department: $department1
+                )",
+                vec![
+                    RoleAssignment {
+                        role: "employee".to_string(),
+                        entity: "rust_dev".to_string(),
+                    },
+                    RoleAssignment {
+                        role: "department".to_string(),
+                        entity: "department1".to_string(),
+                    },
+                ],
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_role_assignments(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
 }
