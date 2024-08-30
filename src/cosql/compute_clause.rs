bb300e7a3c319a6e8db05a2cc360c1434abe422c
Dilshad <a-rustacean@outlook.com>
2024-08-30 08:05:58 +0530

    chore(cosql/compute_clause): add tests for `ComputeClause` parser

--- src/cosql/compute_clause.rs
+++ src/cosql/compute_clause.rs
@@ -15,7 +15,7 @@ use super::{
 
 pub type ComputeClauses = Vec<ComputeClause>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ComputeClause {
     pub variable: String,
     pub expression: Expression,
@@ -40,4 +40,89 @@ pub fn parse_compute_clause(input: &str) -> IResult<&str, ComputeClause> {
             expression,
         },
     )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cosql::{
+        expression::{BinaryExpression, BinaryExpressionOperator},
+        Value,
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_compute_clause_parser() {
+        let values = [
+            (
+                "$profit = $selling_price - $cost_price",
+                ComputeClause {
+                    variable: "profit".to_string(),
+                    expression: Expression::BinaryExpression(Box::new(BinaryExpression {
+                        left: Expression::Value(Value::Variable("selling_price".to_string())),
+                        operator: BinaryExpressionOperator::Subtraction,
+                        right: Expression::Value(Value::Variable("cost_price".to_string())),
+                    })),
+                },
+            ),
+            (
+                "$profit_percentage = ($profit / $cost_price) * 100",
+                ComputeClause {
+                    variable: "profit_percentage".to_string(),
+                    expression: Expression::BinaryExpression(Box::new(BinaryExpression {
+                        left: Expression::BinaryExpression(Box::new(BinaryExpression {
+                            left: Expression::Value(Value::Variable("profit".to_string())),
+                            operator: BinaryExpressionOperator::Division,
+                            right: Expression::Value(Value::Variable("cost_price".to_string())),
+                        })),
+                        operator: BinaryExpressionOperator::Multiplication,
+                        right: Expression::Value(Value::Int(100)),
+                    })),
+                },
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_compute_clause(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
+
+    #[test]
+    fn test_compute_clauses_parser() {
+        let values = [(
+            "compute
+                $profit = $selling_price - $cost_price,
+                $profit_percentage = ($profit / $cost_price) * 100",
+            vec![
+                ComputeClause {
+                    variable: "profit".to_string(),
+                    expression: Expression::BinaryExpression(Box::new(BinaryExpression {
+                        left: Expression::Value(Value::Variable("selling_price".to_string())),
+                        operator: BinaryExpressionOperator::Subtraction,
+                        right: Expression::Value(Value::Variable("cost_price".to_string())),
+                    })),
+                },
+                ComputeClause {
+                    variable: "profit_percentage".to_string(),
+                    expression: Expression::BinaryExpression(Box::new(BinaryExpression {
+                        left: Expression::BinaryExpression(Box::new(BinaryExpression {
+                            left: Expression::Value(Value::Variable("profit".to_string())),
+                            operator: BinaryExpressionOperator::Division,
+                            right: Expression::Value(Value::Variable("cost_price".to_string())),
+                        })),
+                        operator: BinaryExpressionOperator::Multiplication,
+                        right: Expression::Value(Value::Int(100)),
+                    })),
+                },
+            ],
+        )];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_compute_clauses(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
 }
