348278ead81251744568c1625f333970692c0ab5
mohamedeliwa <mohamed.av11@gmail.com>
2024-09-07 02:19:37 +0300

    created initial repo methods to get and create a vector

--- src/api/vectordb/vectors/repo.rs
+++ src/api/vectordb/vectors/repo.rs
@@ -1,15 +1,15 @@
-use super::{dtos::CreateVectorDto, error::VectorsError, repo};
+use super::{dtos::CreateVectorDto, error::VectorsError};
 
 pub(crate) async fn create_vector(
     collection_id: &str,
     create_vector_dto: CreateVectorDto,
 ) -> Result<String, VectorsError> {
-    repo::create_vector(collection_id, create_vector_dto).await
+    Err(VectorsError::NotFound)?
 }
 
 pub(crate) async fn get_vector_by_id(
     collection_id: &str,
     vector_id: &str,
 ) -> Result<String, VectorsError> {
-    repo::get_vector_by_id(collection_id, vector_id).await
+    Err(VectorsError::NotFound)?
 }
