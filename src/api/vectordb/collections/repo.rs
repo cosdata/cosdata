e4ff6610e13252e066f71598b99883e927dc3a73
mohamedeliwa <mohamed.av11@gmail.com>
2024-09-06 19:15:48 +0300

    created a repo method to delete a collection

--- src/api/vectordb/collections/repo.rs
+++ src/api/vectordb/collections/repo.rs
@@ -56,3 +56,21 @@ pub(crate) fn get_vector_store_by_name(name: &str) -> Result<Arc<VectorStore>, C
     };
     Ok(vec_store)
 }
+
+pub(crate) fn delete_vector_store_by_name(
+    name: &str,
+) -> Result<Arc<VectorStore>, CollectionsError> {
+    let env = match get_app_env() {
+        Ok(env) => env,
+        Err(_) => return Err(CollectionsError::FailedToGetAppEnv),
+    };
+    // Try to get the vector store from the environment
+    let vec_store = match env.vector_store_map.remove(name) {
+        Some((_, store)) => store,
+        None => {
+            // Vector store not found, return an error response
+            return Err(CollectionsError::NotFound);
+        }
+    };
+    Ok(vec_store)
+}
