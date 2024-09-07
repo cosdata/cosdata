cf5c050782ca7ef95cbe30b4cd38ccb1987bf3bf
mohamedeliwa <mohamed.av11@gmail.com>
2024-09-07 14:54:35 +0300

    created a repo method that create a vector

--- src/api/vectordb/vectors/repo.rs
+++ src/api/vectordb/vectors/repo.rs
@@ -1,15 +1,40 @@
-use super::{dtos::CreateVectorDto, error::VectorsError};
+use std::sync::Arc;
+
+use crate::{api::vectordb::collections, api_service::run_upload, config_loader::Config};
+
+use super::{
+    dtos::{CreateVectorDto, CreateVectorResponseDto},
+    error::VectorsError,
+};
 
 pub(crate) async fn create_vector(
     collection_id: &str,
     create_vector_dto: CreateVectorDto,
-) -> Result<String, VectorsError> {
-    Err(VectorsError::NotFound)?
+    config: Arc<Config>,
+) -> Result<CreateVectorResponseDto, VectorsError> {
+    let collection = collections::service::get_collection_by_id(collection_id)
+        .await
+        .map_err(|e| VectorsError::FailedToCreateVector(e.to_string()))?;
+
+    // error cases that happens within run_upload is not handled
+    // this method always return a successfull response with the data sent by the user
+    run_upload(
+        collection,
+        vec![(
+            create_vector_dto.id.clone(),
+            create_vector_dto.values.clone(),
+        )],
+        config,
+    );
+    Ok(CreateVectorResponseDto {
+        id: create_vector_dto.id,
+        values: create_vector_dto.values,
+    })
 }
 
 pub(crate) async fn get_vector_by_id(
-    collection_id: &str,
-    vector_id: &str,
-) -> Result<String, VectorsError> {
+    _collection_id: &str,
+    _vector_id: &str,
+) -> Result<CreateVectorResponseDto, VectorsError> {
     Err(VectorsError::NotFound)?
 }
