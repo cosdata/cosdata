5d8f7950abf0b8f2954c1d59632324f92c54fe7a
Ayush Singh Bhandari <ayush.sb2003@gmail.com>
2024-09-12 11:22:23 +0530

    Use bench_with_input for test

--- benches/inverted_index_benchmark.rs
+++ benches/inverted_index_benchmark.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeSet;
 
 use cosdata::storage::inverted_index::InvertedIndex;
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
 // Function to generate multiple random sparse vectors
@@ -42,7 +42,6 @@ fn benchmark_inserts(c: &mut Criterion) {
     let num_vectors = 100;
     let min_nonzero = 1;
     let max_nonzero = 4;
-    // let perturbation_degree = 0.25;
 
     // Generate random sparse vectors
     let records = generate_random_sparse_vectors(num_vectors, max_index, min_nonzero, max_nonzero);
@@ -50,13 +49,21 @@ fn benchmark_inserts(c: &mut Criterion) {
     // Create new inverted index
     let inverted_index: InvertedIndex<f32> = InvertedIndex::new();
 
-    c.bench_function("Insert 100 sparse vectors of dimensionality 10", |b| {
-        b.iter(|| {
-            for (id, record) in records.iter().enumerate() {
-                let _ = inverted_index.add_sparse_vector(record.to_vec(), id as u32);
-            }
-        });
-    });
+    let test_name = format!(
+        "Insert {} sparse vectors of dimensionality {}",
+        num_vectors, max_index
+    );
+    c.bench_with_input(
+        BenchmarkId::new(test_name, num_vectors),
+        &records,
+        |b, records| {
+            b.iter(|| {
+                for (id, record) in records.iter().enumerate() {
+                    let _ = inverted_index.add_sparse_vector(record.to_vec(), id as u32);
+                }
+            });
+        },
+    );
 }
 
 criterion_group!(benches, benchmark_inserts);
