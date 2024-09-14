c63985210c8246b874ff1c7b76b5b8eeefcf2e95
Dilshad <a-rustacean@outlook.com>
2024-09-14 15:15:35 +0530

    chore: add test for `ExtendEntityInference`

--- src/cosql/inference/extend_entity.rs
+++ src/cosql/inference/extend_entity.rs
@@ -5,7 +5,7 @@ use crate::cosql::{
     insertion::{parse_attributes0, Attributes},
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ExtendEntityInference {
     pub variable: String,
     pub attributes: Attributes,
@@ -19,4 +19,46 @@ pub fn parse_extend_entity_inference(input: &str) -> IResult<&str, ExtendEntityI
             attributes,
         },
     )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cosql::{insertion::Attribute, Value};
+
+    #[test]
+    fn test_extend_entity_inference_parser() {
+        let values = [
+            (
+                "extend $person1 (
+                    salary: 100000
+                )",
+                ExtendEntityInference {
+                    variable: "person1".to_string(),
+                    attributes: vec![Attribute {
+                        name: "salary".to_string(),
+                        value: Value::Int(100000),
+                    }],
+                },
+            ),
+            (
+                "extend $city1 (
+                    visitors: $visitors
+                )",
+                ExtendEntityInference {
+                    variable: "city1".to_string(),
+                    attributes: vec![Attribute {
+                        name: "visitors".to_string(),
+                        value: Value::Variable("visitors".to_string()),
+                    }],
+                },
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_extend_entity_inference(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
 }
