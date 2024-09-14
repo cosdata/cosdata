08ea3a8872a2bf9692e1e037bd4e36809d93fb34
Dilshad <a-rustacean@outlook.com>
2024-09-14 15:15:20 +0530

    chore: add test for `EntityInference` parser

--- src/cosql/inference/entity.rs
+++ src/cosql/inference/entity.rs
@@ -5,7 +5,7 @@ use crate::cosql::{
     insertion::{parse_attributes0, Attributes},
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct EntityInference {
     pub variable: String,
     pub entity_type: String,
@@ -26,4 +26,60 @@ pub fn parse_entity_inference(input: &str) -> IResult<&str, EntityInference> {
             attributes,
         },
     )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cosql::{insertion::Attribute, Value};
+
+    #[test]
+    fn test_entity_inference_parser() {
+        let values = [
+            (
+                r#"$person1 isa person (
+                    name: $name,
+                    age: 18,
+                    gender: "M"
+                )"#,
+                EntityInference {
+                    variable: "person1".to_string(),
+                    entity_type: "person".to_string(),
+                    attributes: vec![
+                        Attribute {
+                            name: "name".to_string(),
+                            value: Value::Variable("name".to_string()),
+                        },
+                        Attribute {
+                            name: "age".to_string(),
+                            value: Value::Int(18),
+                        },
+                        Attribute {
+                            name: "gender".to_string(),
+                            value: Value::String("M".to_string()),
+                        },
+                    ],
+                },
+            ),
+            (
+                r#"$city1 isa city (
+                    name: "New York"  
+                )"#,
+                EntityInference {
+                    variable: "city1".to_string(),
+                    entity_type: "city".to_string(),
+                    attributes: vec![Attribute {
+                        name: "name".to_string(),
+                        value: Value::String("New York".to_string()),
+                    }],
+                },
+            ),
+        ];
+
+        for (source, expected) in values {
+            let (_, parsed) = parse_entity_inference(source).unwrap();
+
+            assert_eq!(parsed, expected);
+        }
+    }
 }
