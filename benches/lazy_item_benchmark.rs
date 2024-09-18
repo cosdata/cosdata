a26225aa0ca367534e88f3e836ff596f5c874aaf
hemanth94 <cosmichemanth94@gmail.com>
2024-09-18 19:45:31 +0530

    Benchmark for retrival in LazyItem {Vec, Map}

--- benches/lazy_item_benchmark.rs
+++ benches/lazy_item_benchmark.rs
@@ -1,82 +1,75 @@
+use cosdata::models::identity_collections::IdentityMapKey;
 use cosdata::models::{
-    lazy_load::{LazyItem, LazyItemMap, LazyItemSet, LazyItemVec},
-    types::SparseVector,
+    lazy_load::{LazyItem, LazyItemMap, LazyItemVec},
     versioning::Hash,
 };
-use cosdata::models::identity_collections::IdentityMapKey;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use rand::{rngs::StdRng, Rng, SeedableRng};
-use std::collections::BTreeSet;
+use rand::Rng;
 
-// Function to generate multiple random sparse vectors
-fn generate_random_sparse_vectors(num_records: usize, dimensions: usize) -> Vec<SparseVector> {
-    let mut rng = StdRng::seed_from_u64(2024);
-    let mut records: Vec<SparseVector> = Vec::with_capacity(num_records);
-
-    for vector_id in 0..num_records {
-        // Calculate the number of non-zero elements (5% to 10% of dimensions)
-        let num_nonzero = rng
-            .gen_range((dimensions as f32 * 0.05) as usize..=(dimensions as f32 * 0.10) as usize);
-        let mut entries: Vec<(u32, f32)> = Vec::with_capacity(num_nonzero);
-
-        // BTreeSet is used to store unique indices of nonzero values in sorted order
-        let mut unique_indices = BTreeSet::new();
-        while unique_indices.len() < num_nonzero {
-            let index = rng.gen_range(0..dimensions);
-            unique_indices.insert(index);
+fn create_lazy_item_vec_list(index_count: u32, lazy_vec_count: u32) -> Vec<LazyItemVec<String>> {
+    let mut res: Vec<LazyItemVec<String>> = Vec::new();
+    for i in 0..lazy_vec_count {
+        let lazy_item_vec: LazyItemVec<String> = LazyItemVec::new();
+        for i in 0..index_count {
+            let version_id = Hash::from(i);
+            let item = LazyItem::new(version_id, i.to_string());
+            lazy_item_vec.insert(i as usize, item);
         }
-
-        // Generate random values for the nonzero indices
-        for dim_index in unique_indices {
-            let value = rng.gen();
-            entries.push((dim_index as u32, value));
-        }
-
-        records.push(SparseVector::new(vector_id as u32, entries));
+        res.push(lazy_item_vec);
     }
+    res
+}
 
-    records
+fn create_lazy_item_map_list(index_count: u32, lazy_map_count: u32) -> Vec<LazyItemMap<String>> {
+    let mut res: Vec<LazyItemMap<String>> = Vec::new();
+    for i in 0..lazy_map_count {
+        let lazy_item_map: LazyItemMap<String> = LazyItemMap::new();
+        for i in 0..index_count {
+            let version_id = Hash::from(i);
+            let item = LazyItem::new(version_id, i.to_string());
+            lazy_item_map.insert(IdentityMapKey::Int(i), item);
+        }
+        res.push(lazy_item_map);
+    }
+    res
+}
+
+fn get_biased_random_index() -> usize {
+    let mut rng = rand::thread_rng();
+    if rng.gen_bool(0.75) {
+        // 75% chance to pick from 0..8
+        rng.gen_range(0..8)
+    } else {
+        // 25% chance to pick from 8..16
+        rng.gen_range(8..16)
+    }
 }
 
 fn lazy_item_benchmark(c: &mut Criterion) {
-    let dimensions = 1000; // Increased dimensions
-    let sparse_vector_count = 10000;
-
     let mut group = c.benchmark_group("Lazy item");
     group.sample_size(10);
 
-    // Generate random sparse vectors
-    let records = generate_random_sparse_vectors(sparse_vector_count, dimensions);
+    let list_lazy_vec = create_lazy_item_vec_list(16, 1000);
+    let list_lazy_map = create_lazy_item_map_list(16, 1000);
 
-    //Sequential inserting sparse_vectors to LazyItemVec
-    group.bench_with_input(
-        BenchmarkId::new("LazyItemVec", sparse_vector_count),
-        &records,
-        |b, records| {
-            b.iter(|| {
-                let lazy_item_vec: LazyItemVec<SparseVector> = LazyItemVec::new();
-                for (index, sparse_vec) in records.iter().enumerate() {
-                    let version_hash = Hash::from(index as u32);
-                    let _ = lazy_item_vec.insert(index, LazyItem::new(version_hash, sparse_vec.clone()));
-                }
-            });
-        },
-    );
+    //Retriving random value between (0..16) from each Lazy vector from a List of 1000 LazyItemVec, with 75% probablity to pick from (0..8)
+    group.bench_function(BenchmarkId::new("LazyItemVec_Retrival", 1000), |b| {
+        b.iter(|| {
+            for lazy_vec in list_lazy_vec.iter() {
+                lazy_vec.get(get_biased_random_index());
+            }
+        });
+    });
 
-    //Sequential inserting sparse_vectors to LazyItemMap
-    group.bench_with_input(
-        BenchmarkId::new("LazyItemMap", sparse_vector_count),
-        &records,
-        |b, records| {
-            b.iter(|| {
-                let lazy_item_map: LazyItemMap<SparseVector> = LazyItemMap::new();
-                for (index, sparse_vec) in records.iter().enumerate() {
-                    let version_hash = Hash::from(index as u32);
-                    let _ = lazy_item_map.insert(IdentityMapKey::String(index.to_string()), LazyItem::new(version_hash, sparse_vec.clone()));
-                }
-            });
-        },
-    );
+    //Retriving random value between (0..16) from each Lazy map from a List of 1000 LazyItemMap, with 75% probablity to pick from (0..8)
+    group.bench_function(BenchmarkId::new("LazyItemMap_Retrival", 1000), |b| {
+        b.iter(|| {
+            for lazy_map in list_lazy_map.iter() {
+                let index = get_biased_random_index();
+                lazy_map.get(&IdentityMapKey::Int(index as u32));
+            }
+        });
+    });
 }
 
 criterion_group!(benches, lazy_item_benchmark);
