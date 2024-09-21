2edfe11f9f5655aaf1d42992fd29c15045616f49
Mohamed Eliwa <32942508+mohamedeliwa@users.noreply.github.com>
2024-09-21 12:16:52 +0300

    Search similar vectors api (#116)
    * registering endpont to get similar vectors , creating controller, service, and repo methods, response dto
    
    * created a dto for find similar vectors, added a new variant of vector errors
    
    * modified the org documentation file

--- src/api/vectordb/vectors/mod.rs
+++ src/api/vectordb/vectors/mod.rs
@@ -9,6 +9,7 @@ mod service;
 pub(crate) fn vectors_module() -> Scope {
     let vectors_module = web::scope("/collections/{collection_id}/vectors")
         .route("", web::post().to(controller::create_vector))
+        .route("/search", web::post().to(controller::find_similar_vectors))
         .route("/{vector_id}", web::get().to(controller::get_vector_by_id))
         .route("/{vector_id}", web::put().to(controller::update_vector_by_id));
 
