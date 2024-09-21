08f6e7b0a7871f5408096550bb4f794c728d12fe
mohamedeliwa <mohamed.av11@gmail.com>
2024-09-21 15:41:08 +0300

    registering an endpoint to commit a transaction

--- src/api/vectordb/transactions/controller.rs
+++ src/api/vectordb/transactions/controller.rs
@@ -9,3 +9,13 @@ pub(crate) async fn create_transaction(
     let transaction = service::create_transaction(&collection_id).await?;
     Ok(HttpResponse::Ok().json(transaction))
 }
+
+pub(crate) async fn commit_transaction(
+    params: web::Path<(String, String)>,
+) -> Result<HttpResponse, TransactionError> {
+    let (collection_id, transaction_id) = params.into_inner();
+    Ok(HttpResponse::Ok().body(format!(
+        "collection {}, transaction {}",
+        collection_id, transaction_id
+    )))
+}
--- src/api/vectordb/transactions/mod.rs
+++ src/api/vectordb/transactions/mod.rs
@@ -18,6 +18,10 @@ pub(crate) use upsert::upsert;
 
 pub(crate) fn transactions_module() -> Scope {
     let transactions_module = web::scope("/collections/{collection_id}/transactions")
-        .route("", web::post().to(controller::create_transaction));
+        .route("", web::post().to(controller::create_transaction))
+        .route(
+            "/{transaction_id}/commit",
+            web::post().to(controller::commit_transaction),
+        );
     transactions_module
 }
