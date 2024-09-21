d5e62bd5eafa2688e677bdcf062caf93079bf156
hemanth94 <cosmichemanth94@gmail.com>
2024-09-21 20:43:29 +0530

    Traversal fn for InvertedIndex

--- src/models/knn_filter_inverted_index.rs
+++ src/models/knn_filter_inverted_index.rs
@@ -1,8 +1,10 @@
-use crate::storage::inverted_index::InvertedIndexItem;
+use crate::storage::inverted_index::{self, InvertedIndex, InvertedIndexItem};
 use std::cmp::Ordering::Equal;
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 use std::ops::Mul;
+
+use super::lazy_load::LazyItem;
 
 pub struct ScoredCandidate(f32, u32); // (score, candidate_id)
 
@@ -88,22 +90,48 @@ where
         dot_product
     }
 
-    // Function to find k-nearest neighbors sorted by distance(dot product) for array of InvertedIndex items.
+    // Function to find k-nearest neighbors sorted by distance(dot product) for an InvertedIndex
     pub fn find_k_nearest_neighbours(
         &self,
         k: usize,
-        items: &[InvertedIndexItem<T>],
+        inverted_index: &mut InvertedIndex<T>,
     ) -> Result<Vec<ScoredCandidate>, String> {
-        if k > items.len() {
+        //Sending the root inverted_index_item from which its children can be fetched recursively using traverse_inverted_index function
+        let all_items_in_inverted_index = Self::traverse_inverted_index(inverted_index.root.get());
+
+        if k > all_items_in_inverted_index.len() {
             return Err(String::from(
-                "K value sent is greater than the number of neighbours, Please check!",
+                "K value sent is greater than the number of neighbours!, Please reduce k number",
             ));
         }
-        let mut scored_candidates: Vec<ScoredCandidate> = items
+
+        let mut scored_candidates: Vec<ScoredCandidate> = all_items_in_inverted_index
             .iter()
             .map(|item| ScoredCandidate(self.calculate_distance(item), item.dim_index))
             .collect();
         scored_candidates.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap_or(Equal));
         Ok(scored_candidates.into_iter().take(k).collect())
+    }
+
+    // Traverses entire [InvertedIndex] and returns all root, children which are explicit
+    pub fn traverse_inverted_index(
+        inverted_index_item: &InvertedIndexItem<T>, //root of InvertedIndex is sent first.
+    ) -> Vec<InvertedIndexItem<T>> {
+        let mut vec_inverted_index_items: Vec<InvertedIndexItem<T>> = Vec::new();
+
+        if !inverted_index_item.implicit {
+            // Collecting each inverted_index_item which is explicit to return in a vec
+            vec_inverted_index_items.push(inverted_index_item.clone());
+            let mut map = inverted_index_item.lazy_children.items.arcshift.clone();
+            for (_, child) in map.get().iter() {
+                if let LazyItem::Valid { data: Some(x), .. } = child {
+                    vec_inverted_index_items.extend(
+                        // Calling function recursively for child inverted_index_items
+                        Self::traverse_inverted_index(x.clone().get()),
+                    );
+                }
+            }
+        }
+        vec_inverted_index_items
     }
 }
