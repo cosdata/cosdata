88b86a338c69a7e89d3d0163174b8ffa58538568
Dilshad <a-rustacean@outlook.com>
2024-11-24 05:51:43 +0530

    chore(bufferd_io): add large file read/write tests

--- src/models/buffered_io.rs
+++ src/models/buffered_io.rs
@@ -447,6 +447,7 @@ mod tests {
 
     use super::*;
     use quickcheck_macros::quickcheck;
+    use rand::Rng;
     use std::thread;
     use tempfile::tempfile;
 
@@ -1205,6 +1206,84 @@ mod tests {
         }
 
         bufman.close_cursor(cursor).unwrap();
+    }
+
+    #[test]
+    fn test_read_large_file() {
+        let mut rng = rand::thread_rng();
+
+        let mut file = create_tmp_file(0, 0).unwrap();
+
+        let written_data = (0..(100 * 1024 * 1024))
+            .map(|_| rng.gen())
+            .collect::<Vec<_>>();
+        file.write_all(&written_data).unwrap();
+
+        let bufman = BufferManager::new(file).unwrap();
+        let cursor = bufman.open_cursor().unwrap();
+
+        let mut read_data = vec![0; 100 * 1024 * 1024];
+        let bytes_read = bufman.read_with_cursor(cursor, &mut read_data).unwrap();
+
+        assert_eq!(bytes_read, read_data.len());
+        assert_eq!(read_data, written_data);
+    }
+
+    #[test]
+    fn test_write_large_file() {
+        let mut rng = rand::thread_rng();
+
+        let file = create_tmp_file(0, 0).unwrap();
+        let bufman = BufferManager::new(file).unwrap();
+        let cursor = bufman.open_cursor().unwrap();
+
+        let written_data = (0..(100 * 1024 * 1024))
+            .map(|_| rng.gen())
+            .collect::<Vec<_>>();
+        bufman.write_with_cursor(cursor, &written_data).unwrap();
+        bufman.flush().unwrap();
+
+        let mut read_data = vec![0; 100 * 1024 * 1024];
+
+        let mut file = bufman.file.write().unwrap();
+        file.seek(SeekFrom::Start(0)).unwrap();
+        file.read_exact(&mut read_data).unwrap();
+
+        for (i, (r, w)) in read_data.into_iter().zip(written_data).enumerate() {
+            assert_eq!(r, w, "mismatch at {}", i);
+        }
+    }
+
+    #[test]
+    fn test_chunked_write_large_file() {
+        let mut rng = rand::thread_rng();
+
+        let file = create_tmp_file(0, 0).unwrap();
+        let bufman = BufferManager::new(file).unwrap();
+        let cursor = bufman.open_cursor().unwrap();
+
+        let written_data = (0..(100 * 1024 * 1024))
+            .map(|_| rng.gen())
+            .collect::<Vec<_>>();
+
+        for i in 0..(100 * 1024) {
+            let chunk = &written_data[(i * 1024)..((i * 1024) + 1024)];
+            bufman.write_with_cursor(cursor, chunk).unwrap();
+            let pos = bufman.cursor_position(cursor).unwrap();
+            assert_eq!(pos, i as u64 * 1024 + 1024);
+        }
+
+        bufman.flush().unwrap();
+
+        let mut read_data = vec![0; 100 * 1024 * 1024];
+
+        let mut file = bufman.file.write().unwrap();
+        file.seek(SeekFrom::Start(0)).unwrap();
+        file.read_exact(&mut read_data).unwrap();
+
+        for (i, (r, w)) in read_data.into_iter().zip(written_data).enumerate() {
+            assert_eq!(r, w, "mismatch at {}", i);
+        }
     }
 
     // Prop test for `get_or_create_region` to check that
