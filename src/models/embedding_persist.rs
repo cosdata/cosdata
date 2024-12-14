f754f3e874f7a3bd03a0d0488715521499b5f242
mohamedeliwa <mohamed.av11@gmail.com>
2024-12-14 12:48:22 +0200

    created a function to write sparse vector to buffer/disk

--- src/models/embedding_persist.rs
+++ src/models/embedding_persist.rs
@@ -56,6 +56,54 @@ pub fn write_embedding(
     Ok(start)
 }
 
+/// Writes a sparse embedding to a buffer and returns the next offset.
+///
+/// This function is responsible for writing a given sparse vector embedding into
+/// a buffer managed by the `BufferManager`. After successfully writing the embedding,
+/// it returns the updated offset, which indicates the position after the newly written
+/// data in the buffer, to allow subsequent writes or reads.
+///
+/// # Arguments
+///
+/// * `bufman` - A reference-counted (`Arc`) `BufferManager` that manages the buffer
+///   where the sparse embedding will be written. The `BufferManager` takes care of
+///   resource management and memory access.
+/// * `emb` - A reference to the `RawSparseVectorEmbedding` to be written into the buffer.
+///   The embedding is expected to be in a raw, sparse vector format.
+///
+/// # Returns
+///
+/// This function returns a `Result` containing the following:
+/// - `u32`: The next offset (in bytes) after the embedding is written, indicating
+///   where the next data can be written in the buffer.
+///
+/// If an error occurs during the write operation, a `WaCustomError` is returned.
+///
+/// # Errors
+///
+/// - Returns a `WaCustomError` if the write operation fails, which could be caused by
+///   issues such as insufficient space in the buffer, invalid embedding data, or other
+///   internal errors.
+pub fn write_sparse_embedding(
+    bufman: Arc<BufferManager>,
+    emb: &RawSparseVectorEmbedding,
+) -> Result<u32, WaCustomError> {
+    // TODO: select a better value for `N` (number of bytes to pre-allocate)
+    let serialized = rkyv::to_bytes::<_, 256>(emb)
+        .map_err(|e| WaCustomError::SerializationError(e.to_string()))?;
+
+    let len = serialized.len() as u32;
+    let cursor = bufman.open_cursor()?;
+
+    let start = bufman.seek_with_cursor(cursor, SeekFrom::End(0))? as u32;
+    bufman.write_u32_with_cursor(cursor, len)?;
+    bufman.write_with_cursor(cursor, &serialized)?;
+
+    bufman.close_cursor(cursor)?;
+
+    Ok(start)
+}
+
 /// Reads a sparse embedding from a buffer and returns it along with the next offset.
 ///
 /// This function is designed to read a sparse vector embedding from a specified
