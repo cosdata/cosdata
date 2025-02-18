79074d4b4b50f92f5929aa6fa7b8afe2eadf25f6
Dilshad <a-rustacean@outlook.com>
2025-02-18 10:05:32 +0530

    fix(web_server): use `Data<AppContext>` for app data instead of `Data<Arc<AppContext>>`

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,31 @@
+pub(crate) mod api;
 mod api_service;
 mod app_context;
-pub mod macros;
-mod models;
-mod vector_store;
-mod web_server;
-pub(crate) mod api;
 pub mod config_loader;
 pub mod cosql;
 pub mod distance;
+pub mod grpc;
 pub mod indexes;
+pub mod macros;
+mod models;
 pub mod quantization;
 pub mod storage;
-pub mod grpc;
+mod vector_store;
+mod web_server;
 
-use std::sync::Arc;
-use crate::{
-    app_context::AppContext,
-    web_server::run_actix_server_with_context,
-};
+use actix_web::web::Data;
 
+use crate::{app_context::AppContext, web_server::run_actix_server_with_context};
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
     let config = config_loader::load_config();
     // Create context
-    let context = Arc::new(AppContext::new(config)?);
+    let context = Data::new(AppContext::new(config)?);
 
     // Start gRPC server
-    let grpc_context = context.clone();
+    let grpc_context = context.clone().into_inner();
     actix_web::rt::spawn(async move {
         const DEFAULT_GRPC_PORT: u16 = 50051;
         if let Err(e) = grpc::server::start_grpc_server(grpc_context, DEFAULT_GRPC_PORT).await {
