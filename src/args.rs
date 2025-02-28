89c995ea4759a4e089af86d2e6c294b0badc054c
Dilshad <a-rustacean@outlook.com>
2025-02-28 15:17:24 +0530

    chore: fix typos

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(version, about)]
 pub struct CosdataArgs {
-    /// The admin key used to encrypt data and sesion tokens.
+    /// The admin key used to encrypt data and session tokens.
     #[arg(long)]
     pub admin_key: String,
 }
